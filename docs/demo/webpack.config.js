7a5c59151b2ac78dc2bab1be3e193acfd3197849
vishnu <vishnu14july@gmail.com>
2020-06-21 19:42:02 +0530

    Added jquery-ui alias in webpack.config.js

--- docs/demo/webpack.config.js
+++ docs/demo/webpack.config.js
@@ -3,56 +3,55 @@ var webpack = require('webpack');
 
 
 module.exports = {
-	entry: ["webpack/hot/dev-server", "./js/index.js"],
-	output: {
-		path: path.join(__dirname, 'js'),
-		filename: "index-gen.js",
-		publicPath: '/js/'
-	},
-	watchOptions: {
-		aggregateTimeout: 300,
-		poll: 1000
-	},
-	devServer: {
-		historyApiFallback: true,
-		hot: true,
-		inline: true,
-		stats: 'errors-only',
-		port: 8081
-	},
-	plugins: [
-		new webpack.ProvidePlugin({
-			$: "jquery",
-			jQuery: "jquery",
-			"window.jQuery": "jquery",
-			d3: "d3",
-			Backbone: "backbone",
-			_: "underscore",
-			SockJS: "sockjs-client",
-			moment: "moment",
-		}),
-		new webpack.HotModuleReplacementPlugin({
-			multiStep: true
-		})
-	],
-	module: {
-		loaders: [
-			{ test: /\.css$/, loader: "style-loader!css-loader" },
-			{
-				test: /\.js$/,
-				include: [
-					path.resolve(__dirname, "js"),
-					path.resolve(__dirname, "node_modules/jif-dashboard"),
-				],
-				loader: 'babel',
-				query: {
-					presets: ['es2015'],
-					plugins: [
-						'transform-promise-to-bluebird',
-						'transform-runtime'
-					],
-				}
-			}
-		]
-	}
+  entry: ['webpack/hot/dev-server', './js/index.js'],
+  output: {
+    path: path.join(__dirname, 'js'),
+    filename: 'index-gen.js',
+    publicPath: '/js/'
+  },
+  watchOptions: {aggregateTimeout: 300, poll: 1000},
+  resolve: {
+    alias: {
+      // bind version of jquery-ui
+      'jquery-ui': 'jquery-ui-dist/jquery-ui.js',
+      // bind to modules;
+      modules: path.join(__dirname, 'node_modules'),
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    stats: 'errors-only',
+    port: 8081
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      d3: 'd3',
+      Backbone: 'backbone',
+      _: 'underscore',
+      SockJS: 'sockjs-client',
+      moment: 'moment',
+    }),
+    new webpack.HotModuleReplacementPlugin({multiStep: true})
+  ],
+  module: {
+    loaders: [
+      {test: /\.css$/, loader: 'style-loader!css-loader'}, {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, 'js'),
+          path.resolve(__dirname, 'node_modules/jif-dashboard'),
+        ],
+        loader: 'babel',
+        query: {
+          presets: ['es2015'],
+          plugins: ['transform-promise-to-bluebird', 'transform-runtime'],
+        }
+      }
+    ]
+  }
 };
